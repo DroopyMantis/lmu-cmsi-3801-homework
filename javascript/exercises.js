1b510994f1377cd6f3d44ce41c15a4d94a2c4b64
c44rson <ccabre11@lion.lmu.edu>
2024-09-12 21:27:50 -0700

    JS 6/6 all tests passed

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -68,3 +68,67 @@ export async function meaningfulLineCount(filename) {
 }
 
 // Write your Quaternion class here
+export class Quaternion {
+  constructor(a, b, c, d) {
+    this.a = a
+    this.b = b
+    this.c = c
+    this.d = d
+    Object.freeze(this)
+  }
+
+  get coefficients() {
+    return [this.a, this.b, this.c, this.d]
+  }
+
+  get conjugate() {
+    return new Quaternion(this.a, -this.b, -this.c, -this.d)
+  }
+
+  plus(other) {
+    return new Quaternion(
+      this.a + other.a,
+      this.b + other.b,
+      this.c + other.c,
+      this.d + other.d
+    )
+  }
+
+  times(other) {
+    return new Quaternion(
+      this.a * other.a - this.b * other.b - this.c * other.c - this.d * other.d,
+      this.a * other.b + this.b * other.a + this.c * other.d - this.d * other.c,
+      this.a * other.c - this.b * other.d + this.c * other.a + this.d * other.b,
+      this.a * other.d + this.b * other.c - this.c * other.b + this.d * other.a
+    )
+  }
+
+  toString() {
+    const terms = []
+    if (this.a !== 0) {
+      terms.push(`${this.a}`)
+    }
+    if (this.b !== 0) {
+      terms.push(`${this.b === 1 ? "" : this.b === -1 ? "-" : this.b}i`)
+    }
+    if (this.c !== 0) {
+      terms.push(`${this.c === 1 ? "" : this.c === -1 ? "-" : this.c}j`)
+    }
+    if (this.d !== 0) {
+      terms.push(`${this.d === 1 ? "" : this.d === -1 ? "-" : this.d}k`)
+    }
+
+    if (terms.length === 0) {
+      return "0"
+    }
+    let result = terms[0]
+    for (let i = 1; i < terms.length; i++) {
+      if (terms[i][0] === "-") result += terms[i]
+      else result += `+${terms[i]}`
+    }
+    if (result[0] === "+") {
+      result = result.slice(1)
+    }
+    return result
+  }
+}
