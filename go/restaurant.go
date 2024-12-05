42b1c495b27f20eca895a568631bf920ca3c1900
c44rson <ccabre11@lion.lmu.edu>
2024-12-04 20:00:43 -0800

    code working

--- go/restaurant.go
+++ go/restaurant.go
@@ -3,13 +3,10 @@ package main
 import (
 	"log"
 	"math/rand"
+	"sync"
+	"sync/atomic"
 	"time"
 )
-
-// A little utility that simulates performing a task for a random duration.
-// For example, calling do(10, "Remy", "is cooking") will compute a random
-// number of milliseconds between 5000 and 10000, log "Remy is cooking",
-// and sleep the current goroutine for that much time.
 
 func do(seconds int, action ...any) {
     log.Println(action...)
@@ -17,5 +14,74 @@ func do(seconds int, action ...any) {
     time.Sleep(time.Duration(randomMillis) * time.Millisecond)
 }
 
-// Implement the rest of the simulation here. You may need to add more imports
-// above.
+type Order struct {
+	id           uint64
+	customerName string
+	reply        chan *Order
+}
+
+var orderID atomic.Uint64
+
+const waiterCapacity = 3
+var waiter = make(chan *Order, waiterCapacity)
+
+const (
+	numCustomers    = 10
+	numCooks        = 3
+	maxMealsPerCust = 5
+)
+
+var customerNames = []string{"Ani", "Bai", "Cat", "Dao", "Eve", "Fay", "Gus", "Hua", "Iza", "Jai"}
+var cookNames = []string{"Remy", "Colette", "Linguini"}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	var wg sync.WaitGroup
+
+	for _, cookName := range cookNames {
+		go cook(cookName)
+	}
+
+	for _, customerName := range customerNames {
+		wg.Add(1)
+		go customer(customerName, &wg)
+	}
+
+	wg.Wait()
+	log.Println("All customers have gone home. The restaurant is closing.")
+}
+
+func cook(name string) {
+	for order := range waiter {
+		do(10, name, "is cooking for", order.customerName)
+		order.reply <- order
+	}
+}
+
+func customer(name string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	mealsEaten := 0
+
+	for mealsEaten < maxMealsPerCust {
+		order := &Order{
+			id:           orderID.Add(1),
+			customerName: name,
+			reply:        make(chan *Order, 1),
+		}
+
+		select {
+		case waiter <- order:
+			log.Println(name, "placed order", order.id)
+			preparedOrder := <-order.reply
+			log.Println(name, "received order", preparedOrder.id)
+			do(2, name, "is eating meal", mealsEaten+1)
+			mealsEaten++
+		case <-time.After(7 * time.Second):
+			log.Println(name, "got tired of waiting and will return later")
+			time.Sleep(time.Duration(2500+rand.Intn(2500)) * time.Millisecond)
+		}
+	}
+
+	log.Println(name, "has finished all meals and is leaving.")
+}
